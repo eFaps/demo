4aff264b9954f0241fa2c68d7e8f91b3d0afcba0
Jan Moxter <jan-moxter@users.noreply.github.com>
2009-05-06 05:41:59 +0000

    - in preperation of making the kernel releaseable by moving it to asub repository the tags are moved to the archive sub repository

---
+++ kernel/src/main/java/org/efaps/db/Checkout.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.db;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.efaps.admin.access.AccessTypeEnums;
+import org.efaps.admin.datamodel.Type;
+import org.efaps.db.transaction.StoreResource;
+import org.efaps.util.EFapsException;
+
+/**
+ * The class is used to checkout a file from a given attribute of an object.
+ *
+ * @author tmo
+ * @version $Id$
+ */
+public class Checkout extends AbstractAction  {
+
+  /////////////////////////////////////////////////////////////////////////////
+  // static variables
+
+  /**
+   * Logging instance used in this class.
+   */
+  private static final Log LOG = LogFactory.getLog(Checkout.class);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // instance variables
+
+  /**
+   * Instance holding the oid of the object which is checked out.
+   *
+   * @see #getInstance
+   */
+  private final Instance instance;
+
+  /**
+   * Buffer used to copy from the input stream to the output stream.
+   *
+   * @see #execute
+   */
+  private byte[] buffer = new byte[1024];
+
+  /**
+   * Stores the file name after pre processing.
+   *
+   * @see #preprocess
+   * @see #getFileName
+   */
+  private String fileName = null;
+
+  /////////////////////////////////////////////////////////////////////////////
+  // constructors
+
+  /**
+   * Constructor with object id as string.
+   *
+   * @param _context  eFaps context for this request
+   * @param _oid      oid of object on which the checkout is made
+   * @param _attrName name of the attribute where the blob is in
+   * @todo rewrite to thrown EFapsException
+   */
+  public Checkout(final String _oid)  {
+    this(new Instance(_oid));
+  }
+
+  /**
+   * Constructor with instance object.
+   *
+   * @param _context  eFaps context for this request
+   * @param _instance instance on which the checkout is made
+   * @param _attrName name of the attribute where the blob is in
+   */
+  public Checkout(final Instance _instance)  {
+    this.instance = _instance;
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // instance methods
+
+  /**
+   * The method is only a dummy method and closes the checkout action. The
+   * method should be called, if in the future the checkout class needs a
+   * call to this method.
+   */
+  public void close()  {
+  }
+
+  /**
+   *
+   */
+  public void preprocess() throws Exception  {
+    Context context = Context.getThreadContext();
+
+    Type type = getInstance().getType();
+    String fileName = type.getProperty(PROPERTY_STORE_ATTR_FILE_NAME);
+
+    SearchQuery query = new SearchQuery();
+    query.setObject(context, getInstance());
+    query.addSelect(context, fileName);
+//    try  {
+      query.executeWithoutAccessCheck();
+      if (query.next())  {
+        Object value = query.get(context, fileName);
+        this.fileName = value.toString();
+      }
+//    } finally  {
+      query.close();
+//    }
+  }
+
+  /**
+   * Executes the checkout.
+   *
+   * @param _out      output stream where to write the file
+   * @throws EFapsException if the current context user has now access to 
+   *         checkout the file out of the eFaps object
+   */
+  public void execute(final OutputStream _out) throws EFapsException  {
+    boolean hasAccess = this.instance.getType()
+          .hasAccess(this.instance, 
+                     AccessTypeEnums.CHECKOUT.getAccessType());
+    if (!hasAccess)  {
+      throw new EFapsException(getClass(), "execute.NoAccess");
+    }
+    executeWithoutAccessCheck(_out);
+  }
+
+  /**
+   * Executes the checkout.
+   *
+   * @param _out      output stream where to write the file
+   * @throws EFapsException if checkout action fails
+   */
+  public void executeWithoutAccessCheck(final OutputStream _out) 
+                                                      throws EFapsException  {
+    Context context = Context.getThreadContext();
+    StoreResource store = null;
+    try  {
+      store = context.getStoreResource(getInstance().getType(), 
+                                       getInstance().getId());
+      store.read(_out);
+      store.commit();
+    } catch (EFapsException e)  {
+      LOG.error("could not checkout " + this.instance, e);
+      throw e;
+    } catch (Throwable e)  {
+      LOG.error("could not checkout " + this.instance, e);
+      throw new EFapsException(getClass(), 
+                               "executeWithoutAccessCheck.Throwable", e);
+    } finally  {
+      if ((store != null) && store.isOpened())  {
+        store.abort();
+      }
+    }
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // instance getter and setter methods
+
+  /**
+   * This is the getter method for instance variable {@link #instance}.
+   *
+   * @return value of instance variable {@link #instance}
+   * @see #instance
+   */
+  protected Instance getInstance()  {
+    return this.instance;
+  }
+
+  /**
+   * This is the getter method for instance variable {@link #fileName}.
+   *
+   * @return the fileName of the instance variable {@link #fileName}.
+   * @see #fileName
+   * @see #setFileName
+   */
+  public String getFileName()  {
+    return this.fileName;
+  }
+}
---
+++ shell/src/main/java/org/efaps/shell/method/update/AccessSetUpdate.java
@@ -0,0 +1,461 @@
+/*
+ * Copyright 2006 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.shell.method.update;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.digester.Digester;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.xml.sax.SAXException;
+
+import org.efaps.admin.datamodel.Type;
+import org.efaps.db.Context;
+import org.efaps.db.Delete;
+import org.efaps.db.Insert;
+import org.efaps.db.Instance;
+import org.efaps.db.SearchQuery;
+import org.efaps.db.Update;
+import org.efaps.util.EFapsException;
+
+/**
+ * @author tmo
+ * @version $Id$
+ * @todo description
+ */
+public class AccessSetUpdate extends AbstractUpdate  {
+
+  /////////////////////////////////////////////////////////////////////////////
+  // enums
+  
+  /**
+   * The enum is used to define the links with all information needed to update
+   * the link information between this access set and the related objects.
+   */
+  private enum Links  {
+    /** Link to access types. */
+    AccessTypes("Admin_Access_AccessSet2Type", 
+                "AccessSetLink", 
+                "Admin_Access_AccessType", "AccessTypeLink"),
+    /** Link to data model types. */
+    DataModelTypes("Admin_Access_AccessSet2DataModelType", 
+                   "AccessSetLink", 
+                   "Admin_DataModel_Type", "DataModelTypeLink"),
+    /** Link to persons. */
+    Persons("Admin_Access_AccessSet2UserAbstract", 
+            "AccessSetLink", 
+            "Admin_User_Person", "UserAbstractLink"),
+    /** Link to roles. */
+    Roles("Admin_Access_AccessSet2UserAbstract", 
+          "AccessSetLink", 
+          "Admin_User_Role", "UserAbstractLink"),
+    /** Link to groups. */
+    Groups("Admin_Access_AccessSet2UserAbstract", 
+           "AccessSetLink", 
+           "Admin_User_Group", "UserAbstractLink");
+
+    /** Name of the link. */
+    final String linkName;
+    /** Name of the parent attribute in the link. */
+    final String parentAttrName;
+    /** Name of the child type */
+    final String childTypeName;
+    /** Name of the child attribute in the link. */
+    final String childAttrName;
+    
+    /** 
+    */
+    Links(final String _linkName,
+          final String _parentAttrName,
+          final String _childTypeName, final String _childAttrName)  {
+      this.linkName = _linkName;
+      this.parentAttrName = _parentAttrName;
+      this.childTypeName = _childTypeName;
+      this.childAttrName = _childAttrName;
+    }
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // static variables
+
+  /**
+   * Logging instance used to give logging information of this class.
+   */
+  private final static Log LOG = LogFactory.getLog(AccessSetUpdate.class);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // instance variables
+
+  /**
+   * All definitions of versions are added to this list.
+   */
+  private List < Definition > definitions = new ArrayList < Definition > ();
+ 
+  /////////////////////////////////////////////////////////////////////////////
+  // constructors
+
+  /**
+   *
+   */
+  public AccessSetUpdate() {
+    super("Admin_Access_AccessSet");
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // instance methods
+
+  /**
+   * Adds one definition of a update for a specific version to all definitions
+   * in {@link #definitions}.
+   *
+   * @param _definition definition to add
+   * @see #definitions
+   */
+  public void addDefinition(final Definition _definition)  {
+    this.definitions.add(_definition);
+  }
+
+  public void updateInDB() throws EFapsException,Exception {
+    Context context = Context.getThreadContext();
+
+    Instance instance = getInstance();
+    for (Definition def : this.definitions)  {
+      Update update = new Update(context, instance);
+      update.add(context, "Name", def.name);
+      update.add(context, "Revision", def.globalVersion 
+                                      + "#" + def.localVersion);
+      update.executeWithoutAccessCheck();
+      setLinksInDB(instance, Links.AccessTypes, def.accessTypes);
+      setLinksInDB(instance, Links.DataModelTypes, def.dataModelTypes);
+      setLinksInDB(instance, Links.Persons, def.persons);
+      setLinksInDB(instance, Links.Roles, def.roles);
+      setLinksInDB(instance, Links.Groups, def.groups);
+    }
+  }
+
+  /**
+   * Sets the links from this object to the given list of objects (with the 
+   * object name) in the eFaps database.
+   *
+   * @param _instance   instance for which the access types must be set
+   * @param _linkType   link to update
+   * @param _objNames   string list of all object names to set for this 
+   *                    object
+   */
+  protected void setLinksInDB(final Instance _instance,
+                              final Links _linkType,
+                              final List < String > _objNames)  
+                                            throws EFapsException,Exception  {
+                                              
+    Context context = Context.getThreadContext();
+    
+    // get ids from current object
+    Map < Long, String > currents = new HashMap < Long, String > ();
+    SearchQuery query = new SearchQuery();
+    query.setExpand(context, 
+                    _instance, 
+                    _linkType.linkName + "\\" + _linkType.parentAttrName);
+    query.addSelect(context, _linkType.childAttrName + ".ID");
+    query.addSelect(context, "OID");
+    query.addSelect(context, _linkType.childAttrName + ".Type");
+    query.executeWithoutAccessCheck();
+    while (query.next())  {
+      Type type = (Type) query.get(context, _linkType.childAttrName + ".Type");
+      if (_linkType.childTypeName.equals(type.getName()))  {
+        currents.put((Long) query.get(context, _linkType.childAttrName + ".ID"),
+                   (String) query.get(context, "OID"));
+      }
+    }
+    query.close();
+
+    // get ids for target
+    Set < Long > targets = new HashSet < Long > ();
+    for (String objName : _objNames)  {
+      query = new SearchQuery();
+      query.setQueryTypes(context, _linkType.childTypeName);
+      query.addWhereExprEqValue(context, "Name", objName);
+      query.addSelect(context, "ID");
+      query.executeWithoutAccessCheck();
+      if (query.next())  {
+        targets.add((Long) query.get(context, "ID"));
+        
+      } else  {
+System.out.println(_linkType.childTypeName + " '" + objName + "' not found!");
+      }
+      query.close();
+    }
+
+    // insert needed new links
+    for (Long target : targets)  {
+      if (currents.get(target) == null)  {
+        Insert insert = new Insert(context, _linkType.linkName);
+        insert.add(context, _linkType.parentAttrName, "" + _instance.getId());
+        insert.add(context, _linkType.childAttrName, "" + target);
+        insert.executeWithoutAccessCheck();
+      } else  {
+        currents.remove(target);
+      }
+    }
+
+    // remove unneeded current links to access types
+    for (String oid : currents.values())  {   
+      Delete del = new Delete(oid);
+      del.executeWithoutAccessCheck();
+    }
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // getter and setter methods
+
+  /**
+   * Returns a string representation with values of all instance variables.
+   *
+   * @return string representation of this access set update
+   */
+  public String toString()  {
+    return new ToStringBuilder(this).
+      appendSuper(super.toString()).
+      append("definitions",     this.definitions).
+      toString();
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // static methods
+
+  public static AccessSetUpdate readXMLFile(final String _fileName) throws IOException  {
+//    } catch (IOException e)  {
+//      LOG.error("could not open file '" + _fileName + "'", e);
+    return readXMLFile(new File(_fileName));
+  }
+
+  public static AccessSetUpdate readXMLFile(final File _file) throws IOException  {
+    AccessSetUpdate ret = null;
+
+    try  {
+      Digester digester = new Digester();
+      digester.setValidating(false);
+      digester.addObjectCreate("access-set", AccessSetUpdate.class);
+
+      digester.addCallMethod("access-set/uuid", "setUUID", 1);
+      digester.addCallParam("access-set/uuid", 0);
+
+      digester.addObjectCreate("access-set/definition", Definition.class);
+      digester.addSetNext("access-set/definition", "addDefinition");
+
+      digester.addCallMethod("access-set/definition/version", "setVersion", 4);
+      digester.addCallParam("access-set/definition/version/application", 0);
+      digester.addCallParam("access-set/definition/version/global", 1);
+      digester.addCallParam("access-set/definition/version/local", 2);
+      digester.addCallParam("access-set/definition/version/mode", 3);
+      
+      digester.addCallMethod("access-set/definition/name", "setName", 1);
+      digester.addCallParam("access-set/definition/name", 0);
+
+      digester.addCallMethod("access-set/definition/access-type", "addAccessType", 1);
+      digester.addCallParam("access-set/definition/access-type", 0);
+
+      digester.addCallMethod("access-set/definition/type", "addDataModelType", 1);
+      digester.addCallParam("access-set/definition/type", 0);
+
+      digester.addCallMethod("access-set/definition/person", "addPerson", 1);
+      digester.addCallParam("access-set/definition/person", 0);
+
+      digester.addCallMethod("access-set/definition/role", "addRole", 1);
+      digester.addCallParam("access-set/definition/role", 0);
+
+      digester.addCallMethod("access-set/definition/group", "addGroup", 1);
+      digester.addCallParam("access-set/definition/group", 0);
+
+      ret = (AccessSetUpdate) digester.parse(_file);
+    } catch (SAXException e)  {
+e.printStackTrace();
+      //      LOG.error("could not read file '" + _fileName + "'", e);
+    }
+    return ret;
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // class for the definitions
+
+  public static class Definition  {
+    
+    ///////////////////////////////////////////////////////////////////////////
+    // instance variables
+
+    /**
+     * Name of the application for which this definition is defined.
+     *
+     * @see #setVersion
+     */
+    private String application = null;
+     
+    /**
+     * Number of the global version of the application.
+     *
+     * @see #setVersion
+     */
+    private long globalVersion = 0;
+
+    /**
+     * Text of the local version of this definition.
+     *
+     * @see #setVersion
+     */
+    private String localVersion = null;
+
+    /**
+     * 
+     *
+     * @see #setVersion
+     */
+    private String mode = null;
+
+    /**
+     * Name of the access set for which this definition is defined.
+     */
+    private String name = null;
+
+    /**
+     * List of all access type to which this access set is assigned to.
+     */
+    private final List < String > accessTypes = new ArrayList < String > ();
+
+    /**
+     * List of all data model types which are assigned to this access set.
+     */
+    private final List < String > dataModelTypes = new ArrayList < String > ();
+
+    /**
+     * List of all person which are assigned to this access set.
+     */
+    private final List < String > persons = new ArrayList < String > ();
+
+    /**
+     * List of all roles which are assigned to this access set.
+     */
+    private final List < String > roles = new ArrayList < String > ();
+
+    /**
+     * List of all groups which are assigned to this access set.
+     */
+    private final List < String > groups = new ArrayList < String > ();
+
+    ///////////////////////////////////////////////////////////////////////////
+    // instance methods
+
+    /**
+     * The version information of this defintion is set.
+     *
+     * @param _application    name of the application for which the version is 
+     *                        defined
+     * @param _globalVersion  global version
+     */
+    public void setVersion(final String _application, 
+                           final String _globalVersion,
+                           final String _localVersion,
+                           final String _mode)  {
+      this.application = _application;
+      this.globalVersion = Long.valueOf(_globalVersion);
+      this.localVersion = _localVersion;
+      this.mode = _mode;
+    }
+    
+    /**
+     * @param _accessType access type to add (defined with the name of the
+     *                    access type)
+     * @see #accessTypes
+     */
+    public void addAccessType(final String _accessType)  {
+      this.accessTypes.add(_accessType);
+    }
+
+    /**
+     * @param _dataModelType  data model type to add (defined with the name of
+     *                        the data model type)
+     * @see #dataModelTypes
+     */
+    public void addDataModelType(final String _dataModelType)  {
+      this.dataModelTypes.add(_dataModelType);
+    }
+
+    /**
+     * @param _person person to add (defined with the name of the person)
+     * @see #persons
+     */
+    public void addPerson(final String _person)  {
+      this.persons.add(_person);
+    }
+
+    /**
+     * @param _role role to add (defined with the name of the role)
+     * @see #roles
+     */
+    public void addRole(final String _role)  {
+      this.roles.add(_role);
+    }
+
+    /**
+     * @param _group group to add (defined with the name of the group)
+     * @see #groups
+     */
+    public void addGroup(final String _group)  {
+      this.groups.add(_group);
+    }
+
+    /**
+     *
+     * @param _name name of the access set (for this version definition)
+     * @see #name
+     */
+    public void setName(final String _name)  {
+      this.name = _name;
+    }
+
+    /**
+     * Returns a string representation with values of all instance variables
+     * of a definition.
+     *
+     * @return string representation of this definition of an access set update
+     */
+    public String toString()  {
+      return new ToStringBuilder(this).
+        append("application",     this.application).
+        append("global version",  this.globalVersion).
+        append("local version",   this.localVersion).
+        append("mode",            this.mode).
+        append("name",            this.name).
+        append("access types",    this.accessTypes).
+        append("persons",         this.persons).
+        append("roles",           this.roles).
+        append("groups",          this.groups).
+        toString();
+    }
+  }
+}
